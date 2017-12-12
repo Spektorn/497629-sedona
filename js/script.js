e3a5c73f66dbb98c6ac0c7775addf19c3487ce02
Spektorn <filonenko.zlata@mail.ru>
2017-12-12 23:12:11 +0300

    Подготовка к сдаче #4

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ var btn = document.querySelector(".search-btn");
 var popup = document.querySelector(".search");
 var arrivaldate = popup.querySelector("[name=arrival-date]");
 
+popup.classList.toggle("search-hidden");
+
 btn.addEventListener("click", function (evt) {
     evt.preventDefault();
+    popup.classList.add("search-anim");
     popup.classList.toggle("search-hidden");
-    arrivaldate.focus();
   });
